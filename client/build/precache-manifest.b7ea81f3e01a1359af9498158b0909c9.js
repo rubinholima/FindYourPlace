89ef130f36c81ac2c2dca06514f22b7a84a1d686
Rubinho Lima <rubinholima@me.com>
2020-03-12 21:47:01 -0400

    changed final

---
+++ client/build/precache-manifest.b7ea81f3e01a1359af9498158b0909c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66690f27a823450220e2cce6bf545cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508a37823b9ac85b526c",
+    "url": "/static/css/main.aae01c64.chunk.css"
+  },
+  {
+    "revision": "1286a2b8402abd64dc14",
+    "url": "/static/js/2.c68819e6.chunk.js"
+  },
+  {
+    "revision": "354b6db2b07d14eab39aa51a9e671a5e",
+    "url": "/static/js/2.c68819e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508a37823b9ac85b526c",
+    "url": "/static/js/main.d38a7d3d.chunk.js"
+  },
+  {
+    "revision": "f07d7315372fbd46217f",
+    "url": "/static/js/runtime-main.f09828b8.js"
+  }
+]);
